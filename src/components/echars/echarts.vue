973412137efc2fefd60b7ac55b29dd60a38de3b8
WYY0103 <3177620483@qq.com>
2022-03-29 11:23:57 +0800

    图表绘制

---
+++ src/components/echars/echarts.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        111111
+    </div>
+</template>
+
+
+<script>
+export default {
+    data(){
+
+    },
+    mounted(){
+
+    },
+    methods:{
+
+    }
+}
+</script>
+
+
+<style lang="less" scoped>
+
+</style>
